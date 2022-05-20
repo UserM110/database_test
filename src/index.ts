e823da342ad1fc6e3c014ba2f0bac3eac8524df7
UserM110 <mona_hendy11@yahoo.com>
2022-05-20 02:04:08 +0200

    add middleware

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import express, {Application, Request, Response} from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
 
 const PORT = 3000;
+//server
 const app: Application = express();
-
+//middleware
+app.use(express.json());
+//http
+app.use(morgan('common'));
+//security
+app.use(helmet())
+//routing
 app.get('/', (req: Request, res: Response) => {
     res.json({
         message: "Hello World",
     });
 });
-
+//post
+app.post('/', (req: Request, res: Response) => {res.json({
+    message: "Hello World",
+    data: req.body,
+});
+});
 app.listen(PORT, () => {console.log(`Server is starting at prot:${PORT}`);
 });
 
